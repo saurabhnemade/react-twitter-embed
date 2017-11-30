2552ff4022a3bdd7f88a85da347c58b51b210ab3
saurabhnemade <saurabhnemade@users.noreply.github.com>
2017-12-01 01:26:45 +0530

    api method implementation

--- src/components/TwitterHashtagButton.js
+++ src/components/TwitterHashtagButton.js
@@ -6,20 +6,35 @@ const script = require('scriptjs');
 script('https://platform.twitter.com/widgets.js', 'twitter-embed');
 
 export default class TwitterHashtagButton extends Component {
-  static propTypes = {
-    /**
-     * Tag name for hashtag button
-     */
-    tag: PropTypes.string.isRequired,
-    /**
-     * Additional options to be added to the button
-     */ 
-    options: PropTypes.object
-  };
+    static propTypes = {
+        /**
+         * Tag name for hashtag button
+         */
+        tag: PropTypes.string.isRequired,
+        /**
+         * Additional options to be added to the button
+         */
+        options: PropTypes.object
+    };
 
-  render() {
-    return (
-        <div ref="embedContainer"></div>
-    );
-  }
+    componentDidMount() {
+        script.ready('twitter-embed', () => {
+            if (!window.twttr) {
+                console.error('Failure to load window.twttr, aborting load.');
+                return;
+            }
+
+            window.twttr.widgets.createHashtagButton(
+                this.props.tag,
+                this.refs.embedContainer,
+                this.props.options
+            )
+        });
+    }
+
+    render() {
+        return (
+            <div ref="embedContainer"></div>
+        );
+    }
 }
