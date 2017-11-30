946faebf4567003aa6d33e286007a6f5a68f18f7
saurabhnemade <saurabhnemade@users.noreply.github.com>
2017-12-01 01:53:50 +0530

    props for mention button added

--- src/components/TwitterMentionButton.js
+++ src/components/TwitterMentionButton.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class TwitterMentionButton extends Component {
+  static propTypes = {
+    screenName: PropTypes.string.isRequired,
+    options: PropTypes.object,
+  };
+
   render() {
     return (
-      <div>
-        sample html
-      </div>
+      <div ref="embedContainer" />
     );
   }
 }
