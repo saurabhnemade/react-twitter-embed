dc15c56e98bc2299036557d3f55811a18a157dde
saurabhnemade <saurabhnemade@users.noreply.github.com>
2017-12-01 00:46:27 +0530

    share button props added and render method is updated for reference

--- src/components/TwitterShareButton.js
+++ src/components/TwitterShareButton.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class TwitterShareButton extends Component {
-  render() {
-    return (
-      <div>Sample html</div>
-    );
-  }
+    static propTypes = {
+        /**
+         * Url for sharing
+         */
+        url: PropTypes.string.isRequired,
+        /**
+         * Additional options for overriding config. Details at : https://dev.twitter.com/web/tweet-button/parameters
+         */
+        options: PropTypes.object
+    };
+
+    render() {
+        return (
+            <div ref="embedContainer"></div>
+        );
+    }
 }
