c4890cf225bcc90706f371e101d95e4db6599f01
saurabhnemade <saurabhnemade@users.noreply.github.com>
2017-11-30 22:36:28 +0530

    Config of storybook updated

--- .storybook/config.js
+++ .storybook/config.js
@@ -4,6 +4,13 @@
 // Do not modify this file.
 
 import { configure } from '@kadira/storybook';
+import { setDefaults } from '@storybook/addon-info';
+
+setDefaults({
+  header: false, // Toggles display of header with component name and description
+  inline: false, // Displays info inline vs click button to view
+  source: true,
+});
 
 function loadStories() {
   require('../src/stories');
