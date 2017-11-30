3c406fb07da6683327ccb248c4006f7405ea5f00
saurabhnemade <saurabhnemade@users.noreply.github.com>
2017-12-01 00:58:51 +0530

    twitter share button updated with api implementation

--- src/components/TwitterShareButton.js
+++ src/components/TwitterShareButton.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+const script = require('scriptjs');
+
+script('https://platform.twitter.com/widgets.js', 'twitter-embed');
+
 export default class TwitterShareButton extends Component {
     static propTypes = {
         /**
@@ -12,6 +16,21 @@ export default class TwitterShareButton extends Component {
          */
         options: PropTypes.object
     };
+
+    componentDidMount() {
+        script.ready('twitter-embed', () => {
+            if (!window.twttr) {
+                console.error('Failure to load window.twttr, aborting load.');
+                return;
+            }
+
+            window.twttr.widgets.createShareButton(
+                this.props.url,
+                this.refs.embedContainer,
+                this.props.options
+            )
+        });
+    }
 
     render() {
         return (
