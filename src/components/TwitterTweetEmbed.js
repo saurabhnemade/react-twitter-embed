f08ad1a87f53d8e995ee88961c76d8812fca3ebd
saurabhnemade <saurabhnemade@users.noreply.github.com>
2017-12-01 02:25:01 +0530

    props added for twitter tweet component

--- src/components/TwitterTweetEmbed.js
+++ src/components/TwitterTweetEmbed.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class TwitterTweetEmbed extends Component {
+  static propTypes = {
+    /**
+     * Tweet id that needs to be shown
+     */
+    id: PropTypes.number.isRequired,
+    /**
+     * Additional options to pass to twitter widget plugin
+     */
+    options: PropTypes.object,
+  };
+
   render() {
     return (
       <div>Sample html</div>
