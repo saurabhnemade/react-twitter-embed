19f09c8c5ee6d4cb3d2673944c37b98437db4ca6
saurabhnemade <saurabhnemade@users.noreply.github.com>
2017-12-01 02:41:49 +0530

    tweet component updated to load deps and api calls

--- src/components/TwitterTweetEmbed.js
+++ src/components/TwitterTweetEmbed.js
@@ -1,21 +1,40 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+const script = require('scriptjs');
+
+script('https://platform.twitter.com/widgets.js', 'twitter-embed');
+
 export default class TwitterTweetEmbed extends Component {
   static propTypes = {
-    /**
-     * Tweet id that needs to be shown
-     */
-    id: PropTypes.number.isRequired,
-    /**
-     * Additional options to pass to twitter widget plugin
-     */
+        /**
+         * Tweet id that needs to be shown
+         */
+    tweetId: PropTypes.string.isRequired,
+        /**
+         * Additional options to pass to twitter widget plugin
+         */
     options: PropTypes.object,
   };
 
+  componentDidMount() {
+    script.ready('twitter-embed', () => {
+      if (!window.twttr) {
+        console.error('Failure to load window.twttr, aborting load.');
+        return;
+      }
+
+      window.twttr.widgets.createTweet(
+        this.props.tweetId,
+        this.refs.embedContainer,
+        this.props.options
+      );
+    });
+  }
+
   render() {
     return (
-      <div>Sample html</div>
+      <div ref="embedContainer" />
     );
   }
 }
