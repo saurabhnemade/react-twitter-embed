7306d935bc21378b552d67ed43bc92139d548cab
saurabhnemade <saurabhnemade@users.noreply.github.com>
2017-11-30 22:35:10 +0530

    added basic logic to render the timeline embed frame

--- src/components/TwitterTimelineEmbed.js
+++ src/components/TwitterTimelineEmbed.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
+import PropTypes from "prop-types";
 
 const script = require('scriptjs');
 
 script('https://platform.twitter.com/widgets.js', 'twitter-embed');
 
 export default class TwitterTimelineEmbed extends Component {
-  render() {
-    return (
-      <div>Twitter timeline component</div>
-    );
-  }
+    static propTypes = {
+        sourceType: PropTypes.oneOf(['profile', 'likes', 'list', 'collection', 'URL', 'widget']).isRequired,
+        screenName: PropTypes.string.isRequired,
+        options: PropTypes.options,
+    };
+
+    componentWillMount() {
+        script.ready('twitter-embed', () => {
+            if (!window.twttr) {
+                console.error('Failure to load window.twttr, aborting load.');
+                return;
+            }
+
+            window.twttr.widgets.createTimeline(
+                {
+                    sourceType: this.props.sourceType,
+                    screenName: this.props.screenName
+                },
+                this.refs.embedContainer,
+                this.props.options
+            )
+        });
+    }
+
+    render() {
+        return (
+            <div ref="embedContainer"></div>
+        );
+    }
 }
