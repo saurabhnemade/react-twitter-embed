1e89453fa5ba94dfdc7644f73eff5bea3c0e60bc
saurabhnemade <saurabhnemade@users.noreply.github.com>
2017-12-01 01:22:37 +0530

    Props added for twitter hashtab button

--- src/components/TwitterHashtagButton.js
+++ src/components/TwitterHashtagButton.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class TwitterHashtagButton extends Component {
+  static propTypes = {
+    tag: PropTypes.string.isRequired,
+    options: PropTypes.object
+  };
+
   render() {
     return (
       <div>
