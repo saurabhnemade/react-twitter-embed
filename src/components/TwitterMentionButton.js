ad92934a4b22ac28125351eb880feb02e128695a
saurabhnemade <saurabhnemade@users.noreply.github.com>
2017-12-01 01:56:29 +0530

    Mention button deps added and mount action added

--- src/components/TwitterMentionButton.js
+++ src/components/TwitterMentionButton.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+const script = require('scriptjs');
+
+script('https://platform.twitter.com/widgets.js', 'twitter-embed');
+
 export default class TwitterMentionButton extends Component {
   static propTypes = {
     screenName: PropTypes.string.isRequired,
     options: PropTypes.object,
   };
+
+  componentDidMount() {
+    script.ready('twitter-embed', () => {
+      if (!window.twttr) {
+        console.error('Failure to load window.twttr, aborting load.');
+        return;
+      }
+
+      window.twttr.widgets.createMentionButton(
+        this.props.screenName,
+        this.refs.embedContainer,
+        this.props.options
+      );
+    });
+  }
 
   render() {
     return (
