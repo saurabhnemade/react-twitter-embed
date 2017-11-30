de6f4d2ed827303709ec610d8e5800f9bdc6f2ee
saurabhnemade <saurabhnemade@users.noreply.github.com>
2017-11-30 22:57:57 +0530

    updated for supporting userid and destroy issue

--- src/components/TwitterTimelineEmbed.js
+++ src/components/TwitterTimelineEmbed.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from "prop-types";
+import isRequiredIf from 'react-proptype-conditional-require';
 
 const script = require('scriptjs');
 
@@ -12,16 +13,20 @@ export default class TwitterTimelineEmbed extends Component {
          */
         sourceType: PropTypes.oneOf(['profile', 'likes', 'list', 'collection', 'URL', 'widget']).isRequired,
         /**
-         * username of twitter handle
+         * username of twitter handle as String
          */
-        screenName: PropTypes.string.isRequired,
+        screenName: isRequiredIf(PropTypes.string, props => !props.hasOwnProperty('userId')),
+        /**
+         * UserId of twitter handle as number
+         */
+        userId: isRequiredIf(PropTypes.number, props => !props.hasOwnProperty('screenName')),
         /**
          * Additional options to pass to twitter widget plugin
          */
         options: PropTypes.options,
     };
 
-    componentWillMount() {
+    componentDidMount() {
         script.ready('twitter-embed', () => {
             if (!window.twttr) {
                 console.error('Failure to load window.twttr, aborting load.');
@@ -31,7 +36,8 @@ export default class TwitterTimelineEmbed extends Component {
             window.twttr.widgets.createTimeline(
                 {
                     sourceType: this.props.sourceType,
-                    screenName: this.props.screenName
+                    screenName: this.props.screenName,
+                    userId: this.props.userId
                 },
                 this.refs.embedContainer,
                 this.props.options
