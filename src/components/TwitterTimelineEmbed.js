2c3203cb887c4f2c8d65da4f68fa61157f629dfe
saurabhnemade <saurabhnemade@users.noreply.github.com>
2017-11-30 19:34:21 +0530

    Script is loaded at start of component

--- src/components/TwitterTimelineEmbed.js
+++ src/components/TwitterTimelineEmbed.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 
+const script = require('scriptjs');
+
+script('https://platform.twitter.com/widgets.js', 'twitter-embed');
+
 export default class TwitterTimelineEmbed extends Component {
   render() {
     return (
