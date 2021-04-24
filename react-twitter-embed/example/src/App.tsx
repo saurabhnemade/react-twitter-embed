be580eeb01f2a02d4494442d0bb4fca9c63ec76f
Saurabh Nemade <saurabhnemade@users.noreply.github.com>
2021-04-24 17:55:53 +0530

    initalizing with typescript boilerplate

---
+++ react-twitter-embed/example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'react-twitter-embed'
+import 'react-twitter-embed/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
