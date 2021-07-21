d40a65ebc889ee4213c8ecc126f87083576cd8be
Saurabh Nemade <saurabhnemade@users.noreply.github.com>
2021-07-22 04:26:15 +0530

    dir structure corrected

---
+++ .github/pull_request_template.md
@@ -0,0 +1,10 @@
+
+**PR Checklist**
+
+- [ ] Add feature name in the title
+- [ ] Set the context in the description
+- [ ] Set the appropriate label
+- [ ] Add tests
+- [ ] Relevant screenshots
+- [ ] Fixed a bug? Add it as a test
+- [ ] is a breaking change?
---
+++ src/index.tsx
@@ -0,0 +1,23 @@
+import TwitterTimelineEmbed from './components/TwitterTimelineEmbed'
+import TwitterShareButton from './components/TwitterShareButton'
+import TwitterFollowButton from './components/TwitterFollowButton'
+import TwitterHashtagButton from './components/TwitterHashtagButton'
+import TwitterMentionButton from './components/TwitterMentionButton'
+import TwitterTweetEmbed from './components/TwitterTweetEmbed'
+import TwitterMomentShare from './components/TwitterMomentShare'
+import TwitterDMButton from './components/TwitterDMButton'
+import TwitterVideoEmbed from './components/TwitterVideoEmbed'
+import TwitterOnAirButton from './components/TwitterOnAirButton'
+
+export {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterFollowButton,
+  TwitterHashtagButton,
+  TwitterMentionButton,
+  TwitterTweetEmbed,
+  TwitterMomentShare,
+  TwitterDMButton,
+  TwitterVideoEmbed,
+  TwitterOnAirButton
+};
